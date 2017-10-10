898f5b8a7e1a1e139691d53bbd232d08bbdf54d2
ccowen <caitlincowen2016@u.northwestern.edu>
2017-10-09 20:36:12 -0500

    easy function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -44,30 +44,30 @@ var questionArray = [
 	generateRandomColor()
 ];
 
+function hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
 function easyLevelQuestion(x) {
 
 	var questionColor = questionArray[x];
-
-	function hexToRgb(hex) {
-	    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-	    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-	    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-	        return r + r + g + g + b + b;
-	    });
-
-	    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-	    return result ? {
-	        r: parseInt(result[1], 16),
-	        g: parseInt(result[2], 16),
-	        b: parseInt(result[3], 16)
-	    } : null;
-	}
  
-	randomColorScoreR = hexToRgb(randomColor).r;
-	randomColorScoreG = hexToRgb(randomColor).g;
-	randomColorScoreB = hexToRgb(randomColor).b;
+	randomColorScoreR = hexToRgb(questionColor).r;
+	randomColorScoreG = hexToRgb(questionColor).g;
+	randomColorScoreB = hexToRgb(questionColor).b;
 
-	var questionText = ("Pick the matching color: " + randomColor + " (r" + randomColorScoreR + ", g" + randomColorScoreG + ", b" + randomColorScoreB + ")" );
+	var questionText = ("Pick the matching color: " + questionColor + " (r" + randomColorScoreR + ", g" + randomColorScoreG + ", b" + randomColorScoreB + ")" );
 
 	$("#question").text(questionText);
 
@@ -87,6 +87,7 @@ function easyLevelQuestion(x) {
 		if (x !== generateCorrectAnswerInteger) {
 
 			answerChoices[x] = "testing";
+
 		}
 
 	}
@@ -97,7 +98,11 @@ function easyLevelQuestion(x) {
 	
 	}
 	
-	$("#answers").text(answerChoices);
+	$("#answer1").text(answerChoices[0]);
+	$("#answer2").text(answerChoices[1]);
+	$("#answer3").text(answerChoices[2]);
+	$("#answer4").text(answerChoices[3]);
+	$("#answer5").text(answerChoices[4]);
 
 	
 };
